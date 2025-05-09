39f96a71699e626faf84b3fe51d7c2cb1ed51e9f
sirjust <theoperaking@yahoo.com>
2025-05-08 20:24:39 -0700

    Update build to create assets in root folder

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,9 @@
 const sh = require('shelljs');
 const upath = require('upath');
 
-const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+const destPath = upath.resolve(upath.dirname(__filename), '../.');
 
-sh.rm('-rf', `${destPath}/*`)
-
+sh.rm('-rf', `${destPath}/index.html`)
+sh.rm('-rf', `${destPath}/assets/*`)
+sh.rm('-rf', `${destPath}/css/*`)
+sh.rm('-rf', `${destPath}/js/*`)
