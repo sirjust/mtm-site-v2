c83134deed9d603bfb45645a11c315e6f6046b28
sirjust <theoperaking@yahoo.com>
2025-05-10 17:23:27 -0700

    Updates

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,7 +3,7 @@ const upath = require('upath');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../.');
 
-sh.rm('-rf', `${destPath}/index.html`)
+sh.rm('-rf', `${destPath}index.html`)
 sh.rm('-rf', `${destPath}/assets/*`)
 sh.rm('-rf', `${destPath}/css/*`)
 sh.rm('-rf', `${destPath}/js/*`)
