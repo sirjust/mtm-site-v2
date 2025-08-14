4b989a19e1dc485f5686cab1c14999c51eceea0f
sirjust <theoperaking@yahoo.com>
2025-08-13 21:12:08 -0700

    set up basic validation

---
+++ js/validation.js
@@ -0,0 +1,76 @@
+const validateForm = formSelector => {
+    console.log('Validating form...')
+
+    const formElement = document.querySelector(formSelector);
+
+    const validationOptions = [
+        {
+            attribute: 'required',
+            isValid: input => input.value.trim() !== '',
+            errorMessage: (input, label) => `${label.textContent} is required`
+        },
+        {
+            attribute: 'pattern',
+            isValid: input => {
+                const patternRegex = new RegExp(input.pattern);
+                return patternRegex.test(input.value);
+            },
+            errorMessage: (input, label) => `Not a valid ${label.textContent}`
+        }
+    ]
+
+    const validateSingleFormGroup = formGroup => {
+        const label = formGroup.querySelector('label')
+        const input = formGroup.querySelector('input, textarea');
+        const errorContainer = formGroup.querySelector('.error');
+        const errorIcon = formGroup.querySelector('.error-icon');
+        const successIcon = formGroup.querySelector('.success-icon');
+
+        let formGroupError = false;
+        for(const option of validationOptions){
+            if(input.hasAttribute(option.attribute) && !option.isValid(input)){
+                errorContainer.textContent = option.errorMessage(input, label);
+                input.classList.add('border-red-700')
+                input.classList.remove('border-green-700')
+                successIcon.classList.add('hidden')
+                errorIcon.classList.remove('hidden')
+                formGroupError = true;
+            }
+        }
+
+        if (!formGroupError){
+            errorContainer.textContent = '';
+            input.classList.add('border-green-700')
+            input.classList.remove('border-red-700')
+            successIcon.classList.remove('hidden')
+            errorIcon.classList.add('hidden')
+        }
+
+        return !formGroupError;
+    };
+
+    formElement.setAttribute('novalidate', '');
+
+    formElement.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        if(validateAllFormGroups(formElement)){
+            console.log("Form is valid")
+        } else {
+            console.log("Form is invalid")
+        };
+    });
+
+    const validateAllFormGroups = formToValidate => {
+        const formGroups = Array.from(formToValidate.querySelectorAll('.form-group'));
+
+        var formIsValid = true;
+        formGroups.forEach(formGroup => {
+            if (!validateSingleFormGroup(formGroup))
+                formIsValid = false;
+        });
+        return formIsValid;
+    }
+};
+
+validateForm('#contactForm');
